aa73e223a39aa9cca9528dc6c9cdef13616dc9f1
joao2009 <Gmgc2010@gmail.com>
2022-08-11 19:28:29 -0300

    criando a navbar

---
+++ rainhaelizabeth.js
@@ -0,0 +1,14 @@
+function clicar()
+{
+    pegarvalor = document.getElementById(idade).value
+    if (pegarvalor == 96)
+    {
+        resultado = alert("Acertou! A rainha Elizabeth tem 96 anos!")
+    }
+
+    else{
+        resultado = alert("Nãooooo! A rainha Elizabeth não tem essa idade!")
+    }
+
+
+}
