355cc022e01c3ea7af5c3b858fc3dfdbf1732ec6
JoaoWebMeneses <111091586+JoaoWebMeneses@users.noreply.github.com>
2022-10-25 18:31:26 -0300

    Add files via upload

---
+++ monjacoen.js
@@ -0,0 +1,6 @@
+function r1(){
+    document.getElementById("r1").innerHTML = "A religião onde Monja Coen foi criada é o Cristianismo.";
+}
+function r2(){
+    document.getElementById("r2").innerHTML = "Monja Coen trabalhava como jornalista e repórter.";
+}
