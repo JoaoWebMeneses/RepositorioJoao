e5b20c1305c4505d9ac4adaecc96733eb61687c9
JoaoWebMeneses <111091586+JoaoWebMeneses@users.noreply.github.com>
2022-10-27 14:24:02 -0300

    Add files via upload

--- monjacoen.js
+++ monjacoen.js
@@ -1,6 +1,12 @@
 function r1(){
-    document.getElementById("r1").innerHTML = "A religião onde Monja Coen foi criada é o Cristianismo.";
+    document.getElementById("r1").innerHTML = "A religião que Monja Coen foi criada é o Cristianismo.";
 }
 function r2(){
     document.getElementById("r2").innerHTML = "Monja Coen trabalhava como jornalista e repórter.";
+}
+function r3(){
+    document.getElementById("r3").innerHTML = "Ela entrou nas drogas e já foi presa mais de uma vez."
+}
+function r4(){
+    document.getElementById("r4").innerHTML = "Ela começou a estudar a filosofia Zen nos Estados Unidos e foi para o Japão estudar mais afundo."
 }
