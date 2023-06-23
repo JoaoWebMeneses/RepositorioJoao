083bdff3eaa8c1b9135018d1a61890196f9a6ea9
JoaoWebMeneses <111091586+JoaoWebMeneses@users.noreply.github.com>
2023-06-23 16:41:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-function Cadastrar(){
-    window.alert("Cadastrado meu fih");
-    var qtdInput = document.getElementById("qtd").value;
-    var htmlFinal = "";
-    for (var i = 0; i < qtdInput; i++){
-        htmlFinal += 'Nome: <input> <br/>';
+function Calcular(){
+    var galtura = document.getElementById("altura").value;
+    var gpeso = document.getElementById("peso").value;
+    var caltura = galtura / 100;
+    var imc = gpeso / (caltura * caltura);
+    document.getElementById('imc1').innerHTML = imc.toFixed(2);
+    if (galtura <= 0 || gpeso <= 0){
+        document.getElementById('imc1').innerHTML = '0.00';
+        document.getElementById('res').innerHTML = `Valor inválido!`;
     }
-    document.getElementById('nomes').innerHTML = htmlFinal
-}
-var lista = ['Tomate', 'Queijo', 'Alface']
-function PreencherSelect(){
-    var htmlFinal = '';
-    var tamanhoLista = lista.length;
-    for (var i = 0; i < tamanhoLista; i++){
-        htmlFinal += "<option>"+lista[i] +"</option>"
+    if (imc < 18.5){
+        document.getElementById('res').innerHTML = `Você está com magreza!`;
     }
-    document.getElementById('meuSelect').innerHTML = htmlFinal;
+    else if (imc >= 18.5 && imc < 25){
+        document.getElementById('res').innerHTML = `Você está com peso normal!`;
+    }
+    else if (imc >= 25 && imc < 30){
+        document.getElementById('res').innerHTML = `Você está com sobrepeso!`;
+    }
+    else if (imc >= 30 && imc < 40){
+        document.getElementById('res').innerHTML = `Você está com obesidade!`;
+    }
+    else if (imc >= 40){
+        document.getElementById('res').innerHTML = `Você está com obesidade grave!`;
+    }
 }
 
-function AdicionarItem(){
-    var valorInput = document.getElementById('inputItem').value;
-    lista.push(valorInput);
-    PreencherSelect();
-}
-
-function ApagarItem() {
-    var valorSelecionado = document.getElementById('meuSelect').value;
-    lista.splice(valorSelecionado, 1);
-    PreencherSelect();
+function Limpar(){
+    document.getElementById("altura").value = '';
+    document.getElementById("peso").value = '';
 }
