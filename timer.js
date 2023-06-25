07ce8c408f30f7a30fa1a5676835b7b82f49f78e
JoaoWebMeneses <111091586+JoaoWebMeneses@users.noreply.github.com>
2023-06-25 12:14:37 -0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,15 +1,42 @@
-const seconds = 0
-const minutes = 0
-function Start(){
-    while (true){
-        setTimeout(function(){
-            seconds += 1
-            if(seconds < 10){
-                document.getElementById("seconds").innerHTML = `0${seconds}`;
-            } 
-            else if (seconds >= 10){ 
-                document.getElementById("seconds").innerHTML = seconds;
+let seconds = 0;
+let minutes = 0;
+let startInterval;
+
+function Start() {
+    if (!startInterval) {
+        startInterval = setInterval(function() {
+            seconds++;
+            const secondsElement = document.getElementById("seconds");
+            if (seconds < 10) {
+                secondsElement.innerHTML = `0${seconds}`;
+            } else {
+                secondsElement.innerHTML = seconds;
             }
-        },1000);
+            const minutesElement = document.getElementById("minutes");
+            if (seconds === 60) {
+                seconds = 0;
+                secondsElement.innerHTML = `00`;
+                minutes++;
+            }
+            if (minutes < 10) {
+                minutesElement.innerHTML = `0${minutes}`;
+            } else {
+                minutesElement.innerHTML = minutes;
+            }
+        }, 1000);
     }
+}
+
+function Pause() {
+    clearInterval(startInterval);
+    startInterval = null;
+}
+
+function Stop() {
+    clearInterval(startInterval);
+    startInterval = null;
+    minutes = 0;
+    seconds = 0;
+    document.getElementById("seconds").innerHTML = `0${seconds}`;
+    document.getElementById("minutes").innerHTML = `0${minutes}`;
 }
